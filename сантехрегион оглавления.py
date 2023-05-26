197e26f48535c0126ba376054581550ff4b8523e
павел <118118955+pavel1111pavel@users.noreply.github.com>
2023-05-26 10:25:37 +0300

    Add files via upload

---
+++ сантехрегион оглавления.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+from requests.api import head
+import re
+
+def resp(url):
+    s = requests.Session()
+    response = s.get(url = url)
+     soup = BeautifulSoup(response.content, 'html.parser')
+    x = soup.find_all('ul', class_ = 'tree')
+
+    with open('links.txt', 'w') as f:
+        for i in soup.find_all('ul', class_='tree'):
+            links = i.find_all('a', href=True)
+            for link in links:
+                href = link.get('href')
+                if href:
+                    f.write('http://santeh-region.com' +href + '\n')
+
+
+    with open('сантехрегион sublinks.txt', 'w') as f:
+        with open('links.txt', 'r') as f_links:
+            for line in f_links:
+                link = line.strip()
+                s = requests.Session()
+                response = s.get(url=link)
+                print(response)
+                soup = BeautifulSoup(response.content, 'html.parser')
+                x = soup.find_all('ul', class_='sub-links')
+                for i in x:
+                    hrefs = i.find_all('a', href=True)
+                    for href in hrefs:
+                        link = href.get('href')
+                        if link:
+                            f.write('http://santeh-region.com' + link + '\n')
+                            print('+'*10)
+
+    with open('сантехрегион ID_product.txt', 'w') as id:
+        with open('сантехрегион sublinks.txt', 'r') as f:
+            for line in f:
+                url = line.strip()
+                s = requests.Session()
+                response = s.get(url=url)
+                soup = BeautifulSoup(response.content, 'html.parser')
+                x = soup.find_all('div', class_='image')
+                time.sleep(3)
+                for i in x:
+                    img_src = i.find('img')['src']
+                    match = re.search(r'/(\d+)/images', img_src)
+                    if match:
+
+                        product_id = match.group(1)
+                        id.write(product_id + '\n')
+                        print(product_id)
+
+def main():
+
+    resp(url = 'http://santeh-region.com/')
+
+if __name__ == '__main__':
+    main()
---
+++ сантехрегион.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+from requests.api import head
+import re
+import time
+import openpyxl
+from datetime import date
+today = date.today().strftime("%d-%m-%Y")
+
+def find_row(sheet, product_title, product_id):
+    for row in range(2, sheet.max_row + 1):
+        if sheet.cell(row, 1).value == product_title and sheet.cell(row, 2).value == product_id:
+            return row
+    return None
+
+wb = openpyxl.load_workbook(filename='САНТЕХРЕГИОН Прайс.xlsx')
+sheet = wb.active
+header = ['Title', 'ID']
+if f"Price({today})" not in [cell.value for cell in sheet[1]]:
+    sheet.cell(row=1, column=sheet.max_column + 1).value = f"Price({today})"
+    header.append(f"Price({today})")
+
+with open('сантехрегион ID_product.txt', 'r') as f:
+    data = f.read().splitlines()
+    for i in range(0, len(data), 10):
+
+        list_id = ','.join(data[i:i+10])
+        s = requests.Session()
+        url = f'http://santeh-region.com/compare/{list_id}/'
+        print(url)
+        response = s.get(url=url)
+        time.sleep(5)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        x = soup.find_all('table', class_='compare')
+        #print(x)
+        for i in x:
+            title_list = i.find_all('a', class_='image-link')
+            price_list = i.find_all('span', class_ = 'price nowrap')
+            for title, price in zip(title_list, price_list):
+                img = title.find('img', itemprop='image')
+                product_title = img.get('alt')
+                match = re.search(r'/(\d+)/images', img.get('src'))
+                product_id = int(match.group(1)) if match else None
+                product_price = float(price.get_text().replace(',', '.').replace('Р', '').replace(' ', ''))
+                print(product_title, product_id, product_price)
+                row = find_row(sheet, product_title, product_id)
+                if row:
+                    col = header.index(f"Price({today})") + 1
+                    sheet.cell(row, col).value = product_price
+                else:
+                    row = sheet.max_row + 1
+                    sheet.append([product_title, product_id, product_price])
+
+                time.sleep(1)
+
+wb.save("САНТЕХРЕГИОН Прайс.xlsx")
+
